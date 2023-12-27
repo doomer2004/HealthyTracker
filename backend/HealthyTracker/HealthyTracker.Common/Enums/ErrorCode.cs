fa772124c9aeb441e795e29a9ee9ab2d9dfd80b7
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-12-28 00:04:25 +0200

    Errors

---
+++ backend/HealthyTracker/HealthyTracker.Common/Enums/ErrorCode.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthyTracker.Common.Enums;
+
+public enum ErrorCode
+{
+    ValidationFailed,
+    IdentityError,
+    NotFound,
+    IncorrectParameters,
+    ExternalError,
+    AlreadyExists,
+    ExpiredError
+}
