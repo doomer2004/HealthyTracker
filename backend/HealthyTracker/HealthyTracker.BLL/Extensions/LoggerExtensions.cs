6bc2ea89cfba9afc273e86146c6102357fc91c16
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-12-28 00:03:56 +0200

    Extensions

---
+++ backend/HealthyTracker/HealthyTracker.BLL/Extensions/LoggerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using HealthyTracker.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace HealthyTracker.BLL.Extensions;
+
+public static class LoggerExtensions
+{
+    public static void LogIdentityError<T>(this ILogger<T> logger, User user, IdentityResult result)
+    {
+        if (result.Succeeded)
+        {
+            return;
+        }
+
+        var errors = string.Join("\n", result.Errors.Select(ex => ex.Description));
+        logger.LogError("User with id {1} has following errors:\n{2}", user.Id, errors);
+    }
+}
