e0a8c2f109e29428a4db51c8521891c285de4ef9
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-12-28 00:07:41 +0200

    Update ApplicationDbContext.cs

--- backend/HealthyTracker/HealthyTracker.DAL/Contexts/ApplicationDbContext.cs
+++ backend/HealthyTracker/HealthyTracker.DAL/Contexts/ApplicationDbContext.cs
@@ -10,9 +10,9 @@ public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, 
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     
-    public virtual DbSet<CaloriesConsumed> CaloriesConsumed { get; set; } = null!;
-    public virtual DbSet<CalorieGoal> CalorieGoal { get; set; } = null!;
-    public virtual DbSet<Food> Food { get; set; } = null!;
+    public virtual DbSet<Daily> CaloriesConsumed { get; set; } = null!;
+    public virtual DbSet<NutritionGoal> CalorieGoal { get; set; } = null!;
+    public virtual DbSet<Product> Food { get; set; } = null!;
     public virtual DbSet<Meal> Meal { get; set; } = null!;
     public virtual DbSet<Nutrition> Nutrition { get; set; } = null!;
     public virtual DbSet<User> User { get; set; } = null!;
