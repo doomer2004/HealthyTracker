e524c1d63800c62b7a24bc4cb5396e6aef0986ee
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-12-28 00:01:37 +0200

    Services

---
+++ backend/HealthyTracker/HealthyTracker.BLL/Services/Auth/Auth/AuthService.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using HealthyTracker.BLL.Extensions;
+using HealthyTracker.BLL.Services.Auth.Interfaces;
+using HealthyTracker.Common.Models;
+using HealthyTracker.Common.Models.Configs;
+using HealthyTracker.Common.Models.DTOs.Auth;
+using HealthyTracker.Common.Models.DTOs.Error;
+using HealthyTracker.DAL.Entities;
+using HealthyTracker.Email.Services.Interfaces;
+using LanguageExt;
+using LanguageExt.Common;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace HealthyTracker.BLL.Services.Auth.Auth;
+
+public class AuthService : AuthServiceBase, IAuthService
+{
+    private readonly IMapper _mapper;
+    private readonly IEmailSender _emailSender;
+    private readonly IUserRegistrationService _userRegistrationService;
+    
+    
+    public AuthService(UserManager<User> userManager, JwtConfig jwtConfig, ILogger<AuthServiceBase> logger,
+        IMapper mapper, IEmailSender emailSender, IUserRegistrationService userRegistrationService)
+        : base(userManager, jwtConfig, logger)
+    {
+        _mapper = mapper;
+        _emailSender = emailSender;
+        _userRegistrationService = userRegistrationService;
+    }
+
+    public async Task<Either<ErrorDTO, SignUpResponseDTO>> SignUpAsync(SignUpDTO dto)
+    {
+        var user = await _userManager.FindByEmailAsync(dto.Email);
+        if (user is not null)
+            return new AlreadyExistsErrorDTO("User with this email already exists");
+           
+        
+        user = _mapper.Map<User>(dto);
+        var createdUser = await _userManager.CreateAsync(user, dto.Password);
+        if (!createdUser.Succeeded)
+        {
+            _logger.LogIdentityError(user, createdUser);
+            return new IdentityErrorDTO(
+                "Unable to create a user. Please try again later or use another email address");
+        }
+
+        var generateUrl = await _userRegistrationService.GenerateEmailConfirmationUrlAsync(user.Id);
+        return await generateUrl.Match<Task<Either<ErrorDTO, SignUpResponseDTO>>>(
+            Right: async url =>
+            {
+                var emailSent = await _emailSender.SendEmailAsync(user.Email!,
+                    new EmailConfirmationMessage() {Url = url});
+
+                return emailSent.Match<Either<ErrorDTO, SignUpResponseDTO>>(
+                    None: () =>
+                    {
+                        _logger.LogInformation("Email confirmation url sent to user {0}", user.Id);
+                        return new SignUpResponseDTO {UserId = user.Id};
+                    },
+                    Some: error =>
+                    {
+                        _logger.LogError("Unable to send email confirmation url to user {0}", user.Id);
+                        return new ExternalErrorDTO(error.Message);
+                    });
+            },
+            Left: error =>
+            {
+                _logger.LogError("Unable to generate email confirmation url for user {0}, error: {1}", user.Id,
+                    error.Message);
+
+                return Task.FromResult<Either<ErrorDTO, SignUpResponseDTO>>(
+                    new IncorrectParametersErrorDTO(error.Message));
+            });
+    }
+
+    public async Task<Either<ErrorDTO, AuthSuccessDTO>> SignInAsync(SignInDTO dto)
+    {
+        var user = await _userManager.FindByEmailAsync(dto.Email);
+        if (user is null)
+            return new NotFoundErrorDTO("User with this email does not exist");
+        
+        if (!user.EmailConfirmed)
+            return new IncorrectParametersErrorDTO("Confirm your email before signing in");
+        
+        var validPassword = await _userManager.CheckPasswordAsync(user, dto.Password);
+        if (!validPassword)
+            return new IncorrectParametersErrorDTO("Email or password is incorrect");
+        
+        return await GenerateAuthResultAsync(user);
+    }
+}
---
+++ backend/HealthyTracker/HealthyTracker.BLL/Services/Auth/Auth/EmailConfirmationService.cs
@@ -0,0 +1,94 @@
+﻿using HealthyTracker.BLL.Extensions;
+using HealthyTracker.BLL.Services.Auth.Interfaces;
+using HealthyTracker.Common.Models;
+using HealthyTracker.Common.Models.Configs;
+using HealthyTracker.Common.Models.DTOs.Auth;
+using HealthyTracker.Common.Models.DTOs.Error;
+using HealthyTracker.DAL.Entities;
+using HealthyTracker.Email.Services.Interfaces;
+using LanguageExt;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace HealthyTracker.BLL.Services.Auth.Auth;
+
+public class EmailConfirmationService : AuthServiceBase, IEmailConfirmationService
+{
+    private readonly IEmailSender _emailSender;
+    private readonly IUserRegistrationService _userRegistrationService;
+    public EmailConfirmationService(UserManager<User> userManager, JwtConfig jwtConfig, 
+        ILogger<AuthServiceBase> logger, IEmailSender emailSender, IUserRegistrationService userRegistrationService) 
+        : base(userManager, jwtConfig, logger)
+    {
+        _emailSender = emailSender;
+        _userRegistrationService = userRegistrationService;
+    }
+
+    public async Task<Either<ErrorDTO, AuthSuccessDTO>> ConfirmEmailAsync(ConfirmEmailDTO dto)
+    {
+        var user = await _userManager.FindByIdAsync(dto.UserId.ToString());
+        if (user is null)
+            return new NotFoundErrorDTO("User with this id does not exist");
+
+        if (user.EmailConfirmed)
+            return new IncorrectParametersErrorDTO("Email is already confirmed");
+        
+        var option = await _userRegistrationService.CanConfirmEmailAsync(dto.UserId, dto.Url);
+        return await option.Match<Task<Either<ErrorDTO, AuthSuccessDTO>>>(
+            None: async () =>
+            {
+                user.EmailConfirmed = true;
+                var userUpdated = await _userManager.UpdateAsync(user);
+                if (!userUpdated.Succeeded)
+                {
+                    _logger.LogIdentityError(user, userUpdated);
+                    return new IdentityErrorDTO("Unable to confirm email. Please try again later");
+                }
+
+                _logger.LogInformation("Email confirmed for user: {0}", user.Id);
+                return await GenerateAuthResultAsync(user);
+            },
+            Some: error =>
+            {
+                _logger.LogError("Unable to confirm email for user: {0}, error: {1}", user.Id, error.Message);
+                return Task.FromResult<Either<ErrorDTO, AuthSuccessDTO>>(
+                    new IncorrectParametersErrorDTO(error.Message));
+            });
+    }
+
+    public async Task<Option<ErrorDTO>> ResendConfirmationCodeAsync(ResendConfirmationUrlDTO dto)
+    {
+        var user = await _userManager.FindByIdAsync(dto.UserId.ToString());
+        if (user is null)
+            return new NotFoundErrorDTO("User with this id does not exist");
+
+        var result = await _userRegistrationService.RegenerateEmailConfirmationUrlAsync(dto.UserId);
+        
+        return await result.Match<Task<Option<ErrorDTO>>>(
+            Right: async url =>
+            {
+                var emailSent = await _emailSender.SendEmailAsync(user.Email!,
+                    new EmailConfirmationMessage { Url = url });
+
+                return emailSent.Match<Option<ErrorDTO>>(
+                    None: () =>
+                    {
+                        _logger.LogInformation("Email confirmation code sent to user {0}", user.Id);
+                        return Option<ErrorDTO>.None;
+                    },
+                    Some: error =>
+                    {
+                        _logger.LogError("Unable to send email confirmation code to user {0}", user.Id);
+                        return new ExternalErrorDTO(error.Message);
+                    }
+                );
+            },
+            Left: error =>
+            {
+                _logger.LogError("Unable to generate email confirmation code for user {0}, error: {1}", user.Id,
+                    error.Message);
+
+                return Task.FromResult<Option<ErrorDTO>>(new IncorrectParametersErrorDTO(error.Message));
+            });
+    }
+}
