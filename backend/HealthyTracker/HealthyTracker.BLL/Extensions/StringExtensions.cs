d16ed72314fe2a3ee347b21ae01c9fc6c2181b54
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-12-28 00:03:52 +0200

    Extensions

---
+++ backend/HealthyTracker/HealthyTracker.BLL/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace HealthyTracker.BLL.Extensions;
+
+public static class StringExtensions
+{
+    public static string PathToUrl(this string path, string apiUrl)
+    {
+        var relativePath = path.Replace(@"\", "/").Split("wwwroot/")[1];
+        
+        var url = $"{apiUrl}/{relativePath}";
+
+        return url;
+    }
+    
+    public static string ToAbsolutePath(this string source)
+    {
+        const string solutionName = "HealthyTracker";
+        var path = Directory.GetCurrentDirectory();
+        var solutionPath = path[..path.LastIndexOf(solutionName, StringComparison.Ordinal)];
+        return $"{solutionPath}{source}";
+    }
+}
