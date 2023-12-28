648283d6c823ed545b06a99b916ffcbaa6b66abf
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-12-29 00:13:02 +0200

    DTOs

---
+++ backend/HealthyTracker/HealthyTracker.Common/Models/DTOs/Nutrition/GetNutritionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthyTracker.Common.Models.DTOs.Nutrition;
+
+public class GetNutritionDTO
+{
+    public float Calories { get; set; }
+    public float Protein { get; set; }
+    public float Fat { get; set; }
+    public float Carbs { get; set; }
+}
---
+++ backend/HealthyTracker/HealthyTracker.Common/Models/DTOs/PaginationDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace HealthyTracker.Common.Models.DTOs;
+
+public class PaginationDTO
+{
+    public int Page { get; set; }
+    public int PageSize { get; set; } = 5;
+}
